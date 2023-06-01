c163a3ce41246b26e8ed54b91af1253e25816255
Adam Shiervani <adam.shiervani@gmail.com>
2023-06-01 18:28:23 +0200

    Add delete and fix prettier

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,15 @@
 import * as core from "@actions/core";
-import { wait } from "./wait";
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput("milliseconds");
-    core.debug(`Waiting ${ms} milliseconds ...`); // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString());
-    await wait(parseInt(ms, 10));
-    core.debug(new Date().toTimeString());
-
-    core.setOutput("time", new Date().toTimeString());
+    const cacheKey: string = core.getInput("CACHE_KEY");
+    core.debug(`Will delete ${cacheKey} from the BuildJet Cache`); // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    core.info(`Will delete ${cacheKey} from the BuildJet Cache`); // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    // core.debug(new Date().toTimeString());
+    // await wait(parseInt(ms, 10));
+    // core.debug(new Date().toTimeString());
+    //
+    // core.setOutput("time", new Date().toTimeString());
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message);
   }
