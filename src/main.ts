1515f5e766edbd42425582bc0032414b61556a58
Adam Shiervani <adam.shiervani@gmail.com>
2023-06-01 18:30:45 +0200

    Add delete and fix prettier

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import * as core from "@actions/core";
 
 async function run(): Promise<void> {
   try {
-    const cacheKey: string = core.getInput("CACHE_KEY");
+    const cacheKey: string = core.getInput("cache_key");
     core.debug(`Will delete ${cacheKey} from the BuildJet Cache`); // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
     core.info(`Will delete ${cacheKey} from the BuildJet Cache`); // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
     // core.debug(new Date().toTimeString());
